7c7fd7de101845b1f31f71322f719a6d08eea6ee
alimansoori9575-creator <alimansoori9575@gmail.com>
2026-01-27 16:18:26 +0530

    Git Commands Reference

---
+++ gitdemo/gitcommands.py
@@ -0,0 +1,13 @@
+print("Git Commands Reference") 
+# git log is to show the commit history of the repository.
+# git status is to show the current status of the working directory and staging area.
+# means to show new chenges and stages you have accept or not 
+# git add. will add all new changes at once
+# git add path to add specific file
+# git commit -m "message" to commit the changes with a message 
+# git push to push the changes to the remote repository
+# git pull to pull the latest changes from the remote repository
+# git clone to clone a remote repository to your local machine
+# git branch to show the current branches in the repository
+# git checkout branch_name to switch to a specific branch
+# git merge branch_name to merge a specific branch into the current branch
