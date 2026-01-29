09a590c6751a07f2210ca2cabbf249cf325709d3
alimansoori9575-creator <alimansoori9575@gmail.com>
2026-01-29 14:52:18 +0530

    Some maths in python

---
+++ Sessions note/Math.py
@@ -0,0 +1,53 @@
+# some maths functions
+a = 11
+b = 3
+# addition
+sum = a + b
+print(sum)
+
+# subtraction
+sub = a - b
+print(sub)
+
+# multiplication
+mult = a * b
+print(mult)
+
+# division
+div = a / b
+print(div)  # Output: 3.6666666666666665
+
+# Quotient
+quo = a // b  # Quotient is lower value of division(3.666)   
+print(quo)
+
+# remainder
+rem = a % b
+print(rem)
+
+# exponentiation means power ex: 2 to the power 4
+c = 2
+d = 4
+exp = c ** d
+print(exp)  
+
+# some maths functions using math module
+price = 50
+quantity = 4
+total = price * quantity
+print(total)
+
+total_marks = 450
+sub = 5
+marks_per_subject = total_marks / sub
+print(marks_per_subject)
+
+## Order of exeqution:
+# Parentheses ()
+# Exponents **
+# Multiplication * and Division /
+# Addition + and Subtraction -  
+
+# ex:
+result = (2 + 3) * 4 + 6 / 2 
+print(result)  # Output: 23.0
