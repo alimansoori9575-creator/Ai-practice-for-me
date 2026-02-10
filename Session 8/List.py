e792d5dca6b7e36f445fc56f5acf446c84555110
alimansoori9575-creator <alimansoori9575@gmail.com>
2026-02-10 19:28:58 +0530

    python list

---
+++ Session 8/List.py
@@ -0,0 +1,24 @@
+scores = [85, 92, 78, 96, 88]
+ #         0   1   2   3   4
+# showing index and value
+print(scores[1])
+print(scores[1:3])
+# .append() method is used to add an element to the end of the list
+scores.append(90)
+print(scores[5])
+# .remove() method is to remove something from the list
+scores.remove(92)
+print(scores[1])
+# now how to change index value
+marks = [38, 49, 60, 39, 42, 38, 48]
+marks[3-1] = 40
+print(marks[2])
+
+# list slicing 
+list_1 = marks[0:2]
+list_2 = marks[3:5]
+list_3 = marks[4:]
+print(list_1, list_2, list_3)
+
+# .insert() is for inserting value 
+marks.insert
